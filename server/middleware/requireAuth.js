7b3800cdf66d0f90dac2d6b78243c6d7a1c457a9
kajal-1402-yadav <kajall1402@gmail.com>
2025-09-20 13:20:53 +0530

    Update requireAuth.js

--- server/middleware/requireAuth.js
+++ server/middleware/requireAuth.js
@@ -4,24 +4,23 @@ const User = require('../models/userModel')
 const requireAuth = async (req, res, next) => {
 
     //verify user is authenticated
-
     const { authorization } = req.headers
 
-    if(!authorization){
-        return res.status(401).json({error : 'Authorization token required'})
+    if (!authorization || !authorization.startsWith('Bearer ')) {
+        return res.status(401).json({ error: 'Authorization token required' });
     }
 
     const token = authorization.split(' ')[1]
 
-    try{
+    try {
         const { _id } = jwt.verify(token, process.env.JWT_SECRET)
 
-        req.user = await User.findOne({ _id }).select('_id email')
+        req.user = await User.findById(_id).select('_id email')
         next()
     }
-    catch(error){
+    catch (error) {
         console.log(error)
-        res.status(401).json({error : 'Request is not authorized'})
+        res.status(401).json({ error: 'Request is not authorized' })
     }
 }
 
