f4930fb1f5be479478ca1c85f6291df339bb73fc
kajal-1402-yadav <kajall1402@gmail.com>
2025-09-20 15:36:04 +0530

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,19 +1,11 @@
+const express = require("express")
 const router = express.Router()
-
-const User = require("../models/userModel");
 const { signupUser, loginUser } = require('../controllers/userController');
-
-const app = express()
 
 // Signup 
 router.post('/signup',signupUser);
 
 // Login
 router.post('/login', loginUser);
-
-// Current user
-router.get('/me',(req, res) => {
-    res.json(req.user);
-})
 
 module.exports = router;
